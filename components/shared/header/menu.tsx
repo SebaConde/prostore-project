c77ee523f0d49973bd3f2bcffa64318747390ead
SebaConde <sebaconde7@hotmail.es>
2025-10-08 21:49:41 -0300

    Sign up y Sign out button

--- components/shared/header/menu.tsx
+++ components/shared/header/menu.tsx
@@ -3,6 +3,7 @@ import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { UserIcon, ShoppingCart, EllipsisVertical } from "lucide-react";
 import { SheetContent, SheetDescription, SheetTitle, SheetTrigger,Sheet } from "@/components/ui/sheet";
+import UserButton from "./user-button";
 
 const Menu = () => {
     return ( 
@@ -14,11 +15,7 @@ const Menu = () => {
                         <ShoppingCart /> Cart
                     </Link>
                 </Button>
-                <Button asChild >
-                    <Link href='/sign-in'>
-                        <UserIcon /> Sign in
-                    </Link>
-                </Button>
+                <UserButton />
             </nav>
             <nav className="md:hidden" >
                 <Sheet>
@@ -33,11 +30,7 @@ const Menu = () => {
                                 <ShoppingCart /> Cart
                             </Link>
                         </Button>
-                        <Button asChild >
-                            <Link href='/sign-in'>
-                        <UserIcon /> Sign in
-                            </Link>
-                         </Button>
+                        <UserButton />
                         <SheetDescription></SheetDescription>
                     </SheetContent>
                 </Sheet>
