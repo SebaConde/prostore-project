d8e3d9b3d75c7ac89e10747c239d84505cb79f55
SebaConde <sebaconde7@hotmail.es>
2025-10-24 21:59:25 -0300

    Spinner en producto

--- components/shared/product/add-to-cart.tsx
+++ components/shared/product/add-to-cart.tsx
@@ -2,37 +2,44 @@
 import { Cart, CartItem } from "@/types";
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
-import { Plus, Minus } from "lucide-react";
+import { Plus, Minus, Loader2 } from "lucide-react";
 import { Toaster, toast } from "sonner";
 import { addItemToCart, removeItemFromCart } from "@/lib/actions/cart.actions";
+import { useTransition } from "react";
 
 const AddToCart = ({ cart, item }: { cart?: Cart; item: CartItem }) => {
   const router = useRouter();
 
-  const handleAddToCart = async () => {
-    const res = await addItemToCart(item);
+  const [isPending, startTransition] = useTransition();
 
-    if (!res?.success) {
-      toast.error(`${res.message}`);
-    } else {
-      toast.success(`${res.message}`, {
-        action: {
-          label: "Ver carrito",
-          onClick: () => router.push("/cart"),
-        },
-      });
-    }
+  const handleAddToCart = async () => {
+    startTransition(async () => {
+      const res = await addItemToCart(item);
+
+      if (!res?.success) {
+        toast.error(`${res.message}`);
+      } else {
+        toast.success(`${res.message}`, {
+          action: {
+            label: "Ver carrito",
+            onClick: () => router.push("/cart"),
+          },
+        });
+      }
+    });
   };
-  
+
   const hanldeRemoveFromCart = async () => {
-    const res = await removeItemFromCart(item.productId);
-    toast.warning(`${res.message}`, {
+    startTransition(async () => {
+      const res = await removeItemFromCart(item.productId);
+      toast.warning(`${res.message}`, {
         action: {
           label: "Ver carrito",
           onClick: () => router.push("/cart"),
         },
       });
-  }
+    });
+  };
 
   //Checker si el item esta en el carrito
   const existItem =
@@ -41,18 +48,18 @@ const AddToCart = ({ cart, item }: { cart?: Cart; item: CartItem }) => {
   return existItem ? (
     <div>
       <Button type="button" variant="outline" onClick={hanldeRemoveFromCart}>
-        <Minus className="h-4  w-4"/>
+        {isPending ? (<Loader2 className="w-4 h-4 animate-spin" />): (<Minus className="h-4  w-4"/>)}
       </Button>
       <span className="px-2">{existItem.qty}</span>
       <Button type="button" variant="outline" onClick={handleAddToCart}>
-        <Plus className="h-4  w-4"/>
+        {isPending ? (<Loader2 className="w-4 h-4 animate-spin" />): (<Plus className="h-4  w-4" />)}
       </Button>
     </div>
   ) : (
     <div>
       <Toaster richColors />
       <Button className="w-full" type="button" onClick={handleAddToCart}>
-        <Plus /> add to cart
+        {isPending ? (<Loader2 className="w-4 h-4 animate-spin" />): (<Plus className="h-4  w-4" />)} add to cart
       </Button>
     </div>
   );
