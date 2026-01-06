77263c6f03d6f8bbf7c0925b32e31119a769ea29
SebaConde <sebaconde7@hotmail.es>
2026-01-06 15:43:06 -0300

    Empece a modelar la parte admin

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,5 @@
-import Header from "@/components/shared/header";
-import Footer from "@/components/footer";
+import Header from '@/components/shared/header';
+import Footer from '@/components/footer';
 
 export default function RootLayout({
   children,
@@ -7,10 +7,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-   <div className="flex h-screen flex-col">
-    <Header />
-        <main className="flex-1 wrapper">{children}</main>
-    <Footer />
-   </div>
+    <div className='flex h-screen flex-col'>
+      <Header />
+      <main className='flex-1 wrapper'>{children}</main>
+      <Footer />
+    </div>
   );
 }
